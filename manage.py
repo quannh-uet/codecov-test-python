e12f9b8d48e19162cb4ffa855a70d3807bdf6a57
Nguyen Hong Quan <quannh@datasection.com.vn>
2019-01-18 17:29:02 +0700

    Add circleci and codecov

---
+++ manage.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import sys
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+
+if __name__ == '__main__':
+    if sys.argv[1]:
+        print('Test Pass')
+    else:
+        app.run()
